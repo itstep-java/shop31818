98fa243c4fcdb69d2adad4bbbfbe9c4b043f2973
Itstep <seleznev_k@itstep.org>
2019-07-18 19:40:36 +0300

    Установлен Spring Framework

---
+++ src/main/java/org/itstep/shop31818/Shop31818Application.java
@@ -0,0 +1,13 @@
+package org.itstep.shop31818;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop31818Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop31818Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/itstep/shop31818/Shop31818ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.itstep.shop31818;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Shop31818ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
